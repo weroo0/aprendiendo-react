07a5d187f0495373482c373807fab41f093c6c79
Luisito <hernandezmercadoluisenrique@gmail.com>
2025-06-05 17:08:52 -0600

    [ADD] se crea layout principal en app.jx, se instalo bootstrap y bootstrap-icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css' //Importamos bootstrap
+import 'bootstrap-icons/font/bootstrap-icons.css' //Importamos Bootstrap icons
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
